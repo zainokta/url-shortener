c984aa0d707180f46548b5d4649f476e6d4621aa
zainokta <zainokta@gmail.com>
2024-06-05 01:25:28 +0700

    fix: query escape for password with special character

--- database/postgre.go
+++ database/postgre.go
@@ -3,7 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
-	"log"
+	"net/url"
 	"url-shortener/env"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -12,14 +12,13 @@ import (
 var DB *pgxpool.Pool
 
 func New(ctx context.Context) error {
+	pass := url.QueryEscape(env.GetEnv("DB_PASSWORD", "Master@123"))
 	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s",
 		env.GetEnv("DB_USERNAME", "postgres"),
-		env.GetEnv("DB_PASSWORD", "Master@123"),
+		pass,
 		env.GetEnv("DB_HOST", "localhost:5432"),
 		env.GetEnv("DB_NAME", "link_shortener"),
 	)
-
-	log.Println(dsn)
 
 	conn, err := pgxpool.New(ctx, dsn)
 	if err != nil {
